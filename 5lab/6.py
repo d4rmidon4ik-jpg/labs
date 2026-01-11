a78b067ae9d2b8530c91073726227e90ccb30892
Darmidon4ik <d4rmidon4ik@gmail.com>
2026-01-11 17:56:53 +0300

    zzz

--- 5lab/6.py
+++ 5lab/6.py
@@ -6,12 +6,17 @@ root.title("Прогрессбар")
 
 progress = ttk.Progressbar(root, length=200)
 progress.pack(pady=20)
-
 def start():
+    print "111"
     progress.start(10)
+    i=0
+    while true:
+        print i
+        i+=1
 
 def stop():
     progress.stop()
+    print "123"
 
 tk.Button(root, text="Старт", command=start).pack(side=tk.LEFT, padx=10)
 tk.Button(root, text="Стоп", command=stop).pack(side=tk.RIGHT, padx=10)
--- 8lab/src/8lab/2.py
+++ 8lab/src/8lab/2.py
@@ -2,10 +2,9 @@ import requests
 
 response = requests.get("https://httpbin.org/get")
 print(f"Status: {response.status_code}")
-print(f"JSON: {response.json()}")
 
 try:
-    r = requests.get("https://google.com", timeout=3)
+    r = requests.get("https://google.com")
     print(f"Google доступен: {r.status_code}")
 except requests.exceptions.RequestException as e:
     print(f"Google недоступен: {e}")
